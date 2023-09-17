3966f85a577b5f44d92df283b6fe9f0b7a64ff17
leticiabsilva03 <leticiabsilva03@gmail.com>
2023-09-17 18:51:41 -0300

    feat: decorators

--- angular/c2-typescript-para-angular/src/index.ts
+++ angular/c2-typescript-para-angular/src/index.ts
@@ -173,3 +173,68 @@ const p1 = new Character("KillJoy", 10, 98);
 const p2 = new Magician("Sova", 9, 30, 100);
 p1.skill = 12;
 
+// generics
+function concatArray<T>(...itens: T[]):T[] {
+  return new Array().concat(...itens);
+}
+
+const numArray = concatArray<number[]>([1, 5], [3]);
+const stgArray = concatArray<string[]>(["leticia", "goku"], ["vegeta"]);
+console.log(numArray);
+console.log(stgArray);
+
+let dado: string = "leticia";
+console.log(dado);
+
+// decorators
+function ExibirNome(target1: any){
+  console.log(target1);
+}
+
+@ExibirNome
+class Funcionario {
+
+}
+
+function apiVersion(version: string) {
+  return(target: any) => {
+    Object.assign(target.prototype, {__version: version, __name: "leticia"});
+  };
+}
+
+// atribute decorator 
+function minLength(length: number){
+  return(target: any, key: string) => {
+    let _value = target[key];
+
+    const getter = () => _value;
+    const setter = (value: string) => {
+      if(value.length < length) {
+        throw new Error (`Tamanho menor do que ${length}`);
+      }else {
+        _value = value;
+      }
+    };
+
+    Object.defineProperty (target, key, {
+      get: getter,
+      set: setter,
+    });
+  };
+}
+
+
+
+class Api{
+  @minLength(3)
+  name: string;
+
+  constructor(name: string){
+    this.name = name;
+  }
+}
+
+const api = new Api("usuário");
+console.log(api.name);
+
+
